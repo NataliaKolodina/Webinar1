38d5e4efacb3171126ddb4682b3143fbd79e343a
«Natalia <kolodina_melbyk@mail.ru>
2023-01-26 21:55:51 +0400

    add gitignore

---
+++ task0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()); // из консоля приходят символьный строковый тип данных,переводим в числовой через Int.Parse
+int sqr = number * number;
+Console.WriteLine("Квадрат числа " + number + " равен = " + sqr);
